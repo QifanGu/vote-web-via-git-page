ab949e501bf77e4e0d6fe3297664e427ae0073b3
QifanGu <123200020+QifanGu@users.noreply.github.com>
2024-03-27 15:19:48 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,36 @@
-let trueCount = 0;
-let falseCount = 0;
+// 1. 初始化 Firebase
+var firebaseConfig = {
+    apiKey: "AIzaSyCUaxhfOmvdJoTQULV6s1kYLL7CnHg3y80",
+    authDomain: "web-vote-dbd56.firebaseapp.com",
+    databaseURL: "https://web-vote-dbd56-default-rtdb.firebaseio.com",
+    projectId: "web-vote-dbd56",
+    storageBucket: "web-vote-dbd56.appspot.com",
+    messagingSenderId: "834797078823",
+    appId: "1:834797078823:web:07d297cc68117db6c64102",
+    measurementId: "G-YZWKZESMQD"
+};
+firebase.initializeApp(firebaseConfig);
 
-document.getElementById('trueButton').addEventListener('click', function() {
-    trueCount++;
-    updateChart();
-});
+// 2. 更新数据库的函数
+function vote(option) {
+    var ref = firebase.database().ref('votes/' + option);
+    ref.transaction(function(currentValue) {
+        return (currentValue || 0) + 1;
+    });
+}
 
-document.getElementById('falseButton').addEventListener('click', function() {
-    falseCount++;
-    updateChart();
-});
+document.getElementById('trueButton').addEventListener('click', function() { vote('true'); });
+document.getElementById('falseButton').addEventListener('click', function() { vote('false'); });
 
-const ctx = document.getElementById('myChart').getContext('2d');
-const myChart = new Chart(ctx, {
+// 3. 读取数据并更新图表
+var ctx = document.getElementById('myChart').getContext('2d');
+var myChart = new Chart(ctx, {
     type: 'bar',
     data: {
         labels: ['True', 'False'],
         datasets: [{
-            label: '# of Votes',
-            data: [trueCount, falseCount],
+            label: 'Votes',
+            data: [0, 0], // 初始数据
             backgroundColor: [
                 'rgba(54, 162, 235, 0.2)',
                 'rgba(255, 99, 132, 0.2)'
@@ -39,7 +51,9 @@ const myChart = new Chart(ctx, {
     }
 });
 
-function updateChart() {
-    myChart.data.datasets[0].data = [trueCount, falseCount];
+// 监听数据库中的数据变化，并更新图表
+firebase.database().ref('votes').on('value', (snapshot) => {
+    const data = snapshot.val();
+    myChart.data.datasets[0].data = [data.true || 0, data.false || 0];
     myChart.update();
-}
+});
